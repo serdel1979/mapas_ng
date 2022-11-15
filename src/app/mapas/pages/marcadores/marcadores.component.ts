eb31e75e158454f9da8669ba8e8bdfd492846d69
Sergio De Luca <sdlbsso@gmail.com>
2022-11-15 16:45:04 -0300

    guardado de marcadores en localstorage

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -4,7 +4,8 @@ import * as mapboxgl from 'mapbox-gl';
 
 interface MarcadorColor {
   color: string;
-  marker: mapboxgl.Marker;
+  marker?: mapboxgl.Marker;
+  centro?: [number,number];
 }
 
 
@@ -54,46 +55,67 @@ export class MarcadoresComponent implements AfterViewInit {
       center: this.center, 
       zoom: this.zoomLevel
     });
-
-
-    const markerHtml: HTMLElement = document.createElement('div');
-    markerHtml.innerHTML = 'Mi casa';
-
-    // const marker = new mapboxgl.Marker({
-    //   element: markerHtml
-    // })
-    // .setLngLat(this.center)
-    // .addTo(this.mapa)
-
+    this.leerMarcadoresLocalStorage();
   }
 
   irMarcador(marcador: MarcadorColor){
     this.mapa.flyTo({
-      center: marcador.marker.getLngLat()
+      center: marcador.marker!.getLngLat()
     })
   }
   
   agregarMarcador(){
-
     const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
-    
-    
     const nuevoMarcador = new mapboxgl.Marker({
       draggable: true,
       color
     })
     .setLngLat(this.center)
     .addTo(this.mapa)
-
-
-
     this.marcadores.push({
       color,
       marker: nuevoMarcador
     });
 
-    
-
+    this.guardarMrcadoresLocalStorage();
   }
+
+  guardarMrcadoresLocalStorage(){
+    const lngLatArr: MarcadorColor[]=[];
+    this.marcadores.forEach(m=>{
+      const color = m.color;
+      const { lng, lat } = m.marker!.getLngLat();
+
+      lngLatArr.push({
+        color: color,
+        centro: [ lng, lat ]
+      });
+    })
+
+    localStorage.setItem('marcadores',JSON.stringify(lngLatArr));
+  }
+
+  leerMarcadoresLocalStorage(){
+    if(!localStorage.getItem('marcadores')){
+      return;
+    }
+    const lngLatArr: MarcadorColor[] = JSON.parse(localStorage.getItem('marcadores')!);
+    
+    lngLatArr.forEach(m=>{
+      const newMarker = new mapboxgl.Marker({
+        color: m.color,
+        draggable: true
+      })
+      .setLngLat(m.centro!)
+      .addTo(this.mapa);
+      this.marcadores.push({
+        marker: newMarker,
+        color: m.color,
+      })
+    })
+    
+    
+  }
+
 
 }

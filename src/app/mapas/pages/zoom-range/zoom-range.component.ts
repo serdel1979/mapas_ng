3874306562b56b7085f3a3394c6ee0530a914b1e
Sergio De Luca <sdlbsso@gmail.com>
2022-11-14 23:50:09 -0300

    haciendo zoom

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl'
 
 @Component({
@@ -19,20 +19,58 @@ import * as mapboxgl from 'mapbox-gl'
       position: fixed;
 
       z-index: 999;
+      width: 400px;
     }
   `]
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-    var map = new mapboxgl.Map({
-      container: 'mapa',
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+
+  mapa!: mapboxgl.Map;
+ 
+  zoomLevel: number = 13;
+
+  constructor() {}
+
+
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-57.874025923586096,-34.88778289639234], 
-      zoom: 15
+      zoom: this.zoomLevel
     });
+
+    this.mapa.on('zoom',()=>{
+      this.zoomLevel = this.mapa.getZoom();
+    })
+
+    this.mapa.on('zoom',()=>{
+      if (this.mapa.getZoom() > 18){
+        this.mapa.zoomTo(18);
+      }
+    })
+
+
   }
+
+ 
+
+  zoomOut(){
+    this.mapa.zoomOut();
+  }
+
+  zoomIn(){
+    this.mapa.zoomIn();
+  }
+  
+ 
+  zoomCambio(val: string){
+    this.mapa.zoomTo(Number(val)); 
+  }
+
 
 }

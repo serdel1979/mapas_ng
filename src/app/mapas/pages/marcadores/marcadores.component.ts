505193faa9798980140bd3c4a7d6323753385cd8
unknown <sdlbsso@gmail.com>
2022-11-15 11:01:05 -0300

    Agregado de marcadores

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,15 +1,99 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+
+interface MarcadorColor {
+  color: string;
+  marker: mapboxgl.Marker;
+}
+
+
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
-  styleUrls: ['./marcadores.component.css']
+  styles: [ `
+    
+  .mapa-container{
+    height: 100%;
+    width: 100%;
+  }
+  
+  .list-group{
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    z-index: 99;
+  }
+  
+  li {
+    cursor: pointer;
+  }
+  `]
 })
-export class MarcadoresComponent implements OnInit {
+
+export class MarcadoresComponent implements AfterViewInit {
+
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+ 
+  zoomLevel: number = 13;
+
+  center: [number,number]=[-57.874025923586096,-34.88778289639234];
+
+  marcadores: MarcadorColor[]=[];
 
   constructor() { }
 
-  ngOnInit(): void {
+
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center, 
+      zoom: this.zoomLevel
+    });
+
+
+    const markerHtml: HTMLElement = document.createElement('div');
+    markerHtml.innerHTML = 'Mi casa';
+
+    // const marker = new mapboxgl.Marker({
+    //   element: markerHtml
+    // })
+    // .setLngLat(this.center)
+    // .addTo(this.mapa)
+
+  }
+
+  irMarcador(marcador: MarcadorColor){
+    this.mapa.flyTo({
+      center: marcador.marker.getLngLat()
+    })
+  }
+  
+  agregarMarcador(){
+
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    
+    
+    const nuevoMarcador = new mapboxgl.Marker({
+      draggable: true,
+      color
+    })
+    .setLngLat(this.center)
+    .addTo(this.mapa)
+
+
+
+    this.marcadores.push({
+      color,
+      marker: nuevoMarcador
+    });
+
+    
+
   }
 
 }
--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl'
 
 @Component({
@@ -23,7 +23,7 @@ import * as mapboxgl from 'mapbox-gl'
     }
   `]
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
 
 
@@ -33,27 +33,41 @@ export class ZoomRangeComponent implements AfterViewInit {
  
   zoomLevel: number = 13;
 
+  center: [number,number]=[-57.874025923586096,-34.88778289639234];
+
   constructor() {}
+
+  //cuando hay un listener, hay que destruirlo
+  ngOnDestroy(): void {
+    this.mapa.off('zoom',()=>{});
+    this.mapa.off('zoomend',()=>{});
+    this.mapa.off('move',()=>{});
+  }
 
 
   ngAfterViewInit(): void {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-57.874025923586096,-34.88778289639234], 
+      center: this.center, 
       zoom: this.zoomLevel
     });
 
     this.mapa.on('zoom',()=>{
       this.zoomLevel = this.mapa.getZoom();
-    })
+    });
 
-    this.mapa.on('zoom',()=>{
+    this.mapa.on('zoomend',()=>{
       if (this.mapa.getZoom() > 18){
         this.mapa.zoomTo(18);
       }
-    })
-
+    });
+    
+    this.mapa.on('move',(event)=>{
+      const target = event.target;
+      const {lng, lat} = target.getCenter();
+      this.center = [lng, lat];
+    });
 
   }
 

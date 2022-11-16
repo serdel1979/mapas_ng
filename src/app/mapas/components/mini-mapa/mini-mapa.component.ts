9953e35301ec9cc192d73ab55f431bc12ca36d91
Sergio De Luca <sdlbsso@gmail.com>
2022-11-15 21:09:54 -0300

    mapas chiquitos

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
-  styleUrls: ['./mini-mapa.component.css']
+  styles:[`
+  div {
+    width: 100%;
+    height: 150px;
+    margin: 0px;
+  }
+  `]
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
 
+
+  @Input() lnglat: [number,number] = [0,0];
+
+  @ViewChild('mapa') divMapa!: ElementRef;
   constructor() { }
 
-  ngOnInit(): void {
+
+  ngAfterViewInit(): void {
+    const mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lnglat, 
+      zoom: 15,
+      interactive: false
+    });
+
+    new mapboxgl.Marker()
+    .setLngLat(this.lnglat)
+    .addTo( mapa )
+
   }
+
+  
 
 }
